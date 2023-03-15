3a714b4dc9777a4ff84ca65c92d5a878db24f4e2
Emmanuel <51294607+Tedunjaye@users.noreply.github.com>
2023-03-15 15:41:20 +0100

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -10,7 +10,7 @@ function Card(props) {
       {badgeText && <div className="card--badge">{badgeText}</div>}
       <img src={props.item.photo} className="card--image" />
       <div className="card--stats">
-        <img src="src/pngwing.com.png" className="card--star" />
+        <img src="https://www.istockphoto.com/vector/all-star-gold-gm165722288-11959430" className="card--star" />
         <span>{props.item.rating}</span>
         <span className="gray">({props.item.city})</span>
       </div>
