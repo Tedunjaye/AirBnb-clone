7424b3735a8bdc2d683581b5217318df350da1b6
Emmanuel <51294607+Tedunjaye@users.noreply.github.com>
2023-03-15 15:05:07 +0100

    Add files via upload

---
+++ components/Card.jsx
@@ -0,0 +1,22 @@
+function Card(props) {
+  let badgeText
+  if (props.item.superHost === true) {
+    badgeText = "Super Host"
+  } else {
+    badgeText = "SOLD Out"
+  }
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img src={props.item.photo} className="card--image" />
+      <div className="card--stats">
+        <img src="./src/assets/—Pngtree—golden star_818998.png" className="card--star" />
+        <span>{props.item.rating}</span>
+        <span className="gray">({props.item.city})</span>
+      </div>
+      <span className="gray">{props.item.type} .{props.item.beds} beds</span>
+      <p className="card-title">{props.item.title}</p>
+    </div>
+  )
+}
+export default Card
---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+//import React from "react"
+
+function Navbar() {
+  return (
+    <nav>
+      <p className='logo-heading'>Storm Bnb</p>
+    </nav>
+  )
+}
+
+export default Navbar
